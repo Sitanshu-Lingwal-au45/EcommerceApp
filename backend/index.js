9cad61d4d7df8e3577c48e00b26ff3e4e2e458c6
Sitanshu-lingwal-au45 <slslsingh17@gmail.com>
2022-09-23 10:50:32 +0530

    dep

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,38 @@
-const express = require("express")
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const path = require('path')
 
-const app=express()
-const cors=require('cors')
+const PORT = process.env.PORT || 8000
 
-const PORT=process.env.PORT ||8000
-app.use(cors())
+app.use(cors({
+  origin: '*'
+}))
 
-app.get('/api/products/',(req,res)=>{
-    res.json(products)
-    })
+//console.log(path.join(__dirname, '../front-end/build'))
+const buildPath = path.join(__dirname, '../frontend/build')
+app.use(express.static(buildPath))
 
-app.listen(PORT,()=>{
-    console.log(`Server started on ${PORT}`)
+app.get('/api/products', (req, res) => {
+  res.json(products)
+})
+
+app.get("*", (req, res) => {
+  res.sendFile(`${buildPath}/index.html`)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`)
 })
 
 
-
-const products=[
-    {
-        name: 'P1',
-        price:100
-    },
-    {
-        name:  'P2',
-        price:  300
-
-    }
+const products = [
+  {
+    name: 'P1',
+    price: 100
+  },
+  {
+    name: 'P2',
+    price: 300
+  },
 ]
